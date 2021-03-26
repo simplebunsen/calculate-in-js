9d26fd4a3acf9e8c128ee5904d55f51befd5903c
simplebunsen <youwontget@my.mail>
2021-03-26 20:17:36 +0100

    basic operator functions

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function add(a,b){
+  return a + b;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
+function operate(a,b,operant){
+  switch (operant) {
+    case "+":
+      return add(a,b);
+      break;
+    case "-":
+      return subtract(a,b);
+      break;
+    case "*":
+      return multiply(a,b);
+      break;
+    case "/":
+      return divide(a,b);
+      break;
+  
+    default:
+      break;
+  }
+}
