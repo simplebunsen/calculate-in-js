e25e86960c6dd922b2031b84a08bac2d4d58bfe5
simplebunsen <youwontget@my.mail>
2021-03-29 21:30:18 +0200

    proper indent

--- js/script.js
+++ js/script.js
@@ -38,28 +38,28 @@ function operate(a,b,operator){
 
 }  
 function setUpButtons(){
-    const container = document.querySelector(".calculator-number-container");
-    for (let i = 1; i <= 9; i++) {
-      //create new div and add it
-      const added = container.appendChild(document.createElement("div"));
-      added.innerText = i;
-      added.classList.add("calculator-button");
-    }
+  const container = document.querySelector(".calculator-number-container");
+  for (let i = 1; i <= 9; i++) {
+    //create new div and add it
+    const added = container.appendChild(document.createElement("div"));
+    added.innerText = i;
+    added.classList.add("calculator-button");
   }
+}
 
-  function processNextKeypress(key) {
-    //remove spaces (althought none should be present) and parse key either as Number, or as NaN!
-    const keyAsFloat = parseFloat(key);
+function processNextKeypress(key) {
+  //remove spaces (althought none should be present) and parse key either as Number, or as NaN!
+  const keyAsFloat = parseFloat(key);
 
-    if(Number.isNaN(keyAsFloat)) { //when input cannot be parsed to a number
-      const operatorRegex = new RegExp(" *(\+|\-|\*|\/|\=) *");
-      if (key.match(operatorRegex) !== null) { //if key is an operator
-        //previous Value is the result of current operation
-        previousDisplayNumber = operate(previousDisplayNumber, currentDisplayNumber, previousOperator);
-        currentDisplayNumber = 0;
-        previousOperator = key.trim();
-      }
-    } else { //input is number
-      currentDisplayNumber = currentDisplayNumber * 10 + keyAsFloat;
+  if(Number.isNaN(keyAsFloat)) { //when input cannot be parsed to a number
+    const operatorRegex = new RegExp(" *(\+|\-|\*|\/|\=) *");
+    if (key.match(operatorRegex) !== null) { //if key is an operator
+      //previous Value is the result of current operation
+      previousDisplayNumber = operate(previousDisplayNumber, currentDisplayNumber, previousOperator);
+      currentDisplayNumber = 0;
+      previousOperator = key.trim();
     }
+  } else { //input is number
+    currentDisplayNumber = currentDisplayNumber * 10 + keyAsFloat;
   }
+}
