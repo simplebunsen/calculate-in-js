6f9401f759f7018149c17d3780fdcb0c4b452f7e
simplebunsen <youwontget@my.mail>
2021-03-29 21:29:28 +0200

    refractored operate, edge cases, proper logic (hopefully) in processNextKEyPress

--- js/script.js
+++ js/script.js
@@ -5,33 +5,30 @@ let previousOperator = "";
 function add(a,b){
   return a + b;
 }
-
 function subtract(a,b){
   return a - b;
 }
-
 function multiply(a,b){
   return a * b;
 }
-
 function divide(a,b){
   return a / b;
 }
 
-function operate(a,b,operant){
-  switch (operant) {
+function operate(a,b,operator){
+  switch (operator) {
     case "+":
       return add(a,b);
-      break;
     case "-":
       return subtract(a,b);
-      break;
     case "*":
       return multiply(a,b);
-      break;
     case "/":
       return divide(a,b);
-      break;
+    case "=":
+      return operate(a,b,previousOperator);
+    case "":
+      return currentDisplayNumber;
   
     default:
       //probably not best practice but should work
@@ -43,6 +40,7 @@ function operate(a,b,operant){
 function setUpButtons(){
     const container = document.querySelector(".calculator-number-container");
     for (let i = 1; i <= 9; i++) {
+      //create new div and add it
       const added = container.appendChild(document.createElement("div"));
       added.innerText = i;
       added.classList.add("calculator-button");
@@ -54,9 +52,12 @@ function setUpButtons(){
     const keyAsFloat = parseFloat(key);
 
     if(Number.isNaN(keyAsFloat)) { //when input cannot be parsed to a number
-      const operatorRegex = new RegExp(" *(\+|\-|\*|\/) *");
+      const operatorRegex = new RegExp(" *(\+|\-|\*|\/|\=) *");
       if (key.match(operatorRegex) !== null) { //if key is an operator
-        operate(previousDisplayNumber, currentDisplayNumber, key.trim());
+        //previous Value is the result of current operation
+        previousDisplayNumber = operate(previousDisplayNumber, currentDisplayNumber, previousOperator);
+        currentDisplayNumber = 0;
+        previousOperator = key.trim();
       }
     } else { //input is number
       currentDisplayNumber = currentDisplayNumber * 10 + keyAsFloat;
